9cfed938e39f49460667aa4eb929a05040a07eaf
Fang_Zhijian <3058827925@qq.com>
2022-04-07 17:55:26 +0800

    update

---
+++ src/fang/uniqueuuid/CommandHandler.java
@@ -0,0 +1,33 @@
+package fang.uniqueuuid;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Objects;
+
+public class CommandHandler implements CommandExecutor{
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        FileConfiguration fc = UniqueUUID.instance.getConfig();
+
+        if(strings.length == 0){
+            UniqueUUID.instance.getLogger().info("请使用 /uniqueuuid help 查看命令帮助");
+            return false;
+        }
+        String arg = strings[0];
+        if(Objects.equals(arg, "reload")){
+            UniqueUUID.instance.getLogger().info("开始重置插件...");
+            UniqueUUID.reloadAll();
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
--- src/fang/uniqueuuid/UniqueUUID.java
+++ src/fang/uniqueuuid/UniqueUUID.java
@@ -2,7 +2,9 @@ package fang.uniqueuuid;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.Objects;
 import java.util.logging.Level;
 
 public class UniqueUUID extends JavaPlugin{
@@ -13,10 +15,13 @@ public class UniqueUUID extends JavaPlugin{
 
     public static JavaPlugin instance;
     public static boolean dbError = false;
-
+    public static FileConfiguration fc = instance.getConfig();
     @Override
     public void onEnable() {
+
         getLogger().log(Level.INFO, "启动插件...");
+        Bukkit.getPluginCommand("uniqueuuid").setExecutor(new CommandHandler());
+        //Objects.requireNonNull(Bukkit.getPluginCommand("uniqueuuid")).setExecutor(new CommandHandler());
         Bukkit.getPluginManager().registerEvents(new EventListener(), this);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -30,6 +35,12 @@ public class UniqueUUID extends JavaPlugin{
         instance = this;
 
         new DBDataManager().loadAll();
+    }
+
+    public static void reloadAll() {
+        instance.reloadConfig();
+        new DBDataManager().loadAll();
+        instance.getLogger().info(fc.getString("message.reload"));
     }
 
     @Override
